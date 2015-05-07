dc3414461825ed651f62993997347085186498b3
Max Nowack <max@unsou.de>
2015-05-07 21:51:26 +0200

    fixed documentation

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   git: 'https://github.com/oafmedium/meteor-autoform-select',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  // documentation: 'README.md'
+  documentation: null //'README.md'
 });
 
 Package.onUse(function(api) {
